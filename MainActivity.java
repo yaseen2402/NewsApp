1d44b48385a6a43298c56d937d3bebb191c980f2
yaseen <119795964+yaseen2402@users.noreply.github.com>
2023-06-25 13:24:36 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.news99;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.news99.Model.Articles;
+import com.example.news99.Model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+    final String API_KEY ="89c3845a4ab14a7fbc9427635bc6cbd2";
+    Adapter adapter;
+    List<Articles> articles = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout =findViewById(R.id.swipeRefresh);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String country = getCountry();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retrieveJson(country,API_KEY);
+            }
+        });
+        retrieveJson(country,API_KEY);
+    }
+
+    public void retrieveJson(String country , String apiKey){
+
+        swipeRefreshLayout.setRefreshing(true);
+        Call<Headlines> call = ApiClient.getInstance().getApi().getHeadlines(country,apiKey);
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if(response.isSuccessful() && response.body().getArticles()!=null){
+                    swipeRefreshLayout.setRefreshing(false);
+                    articles.clear();
+                    articles=response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this,articles);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public String getCountry(){
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+
+}
+
